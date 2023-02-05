5ad683246360339269b81da612515747bdb2dede
filipe-monteiro <16304630+filipe-monteiro@users.noreply.github.com>
2023-02-05 17:46:19 +0000

    adding trackDecrement event

--- src/analytics/events.ts
+++ src/analytics/events.ts
@@ -2,3 +2,9 @@ import { trackEvent } from "./trackEvent";
 
 export const trackIncrement = (value: Number) =>
   trackEvent("Counter", "Counter incremented", { value });
+
+
+// Added an event to tract decrement actions as well
+// https://synthesia.notion.site/Analytics-Engineer-Synthesia-Challenge-8d9a05d3967b4ceaae96d52a83b36690
+export const trackDecrement = (value: Number) =>
+  trackEvent("Counter", "Counter decremented", { value });
